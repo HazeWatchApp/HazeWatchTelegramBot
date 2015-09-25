4aedf8eecc01ecd7e51421fef7e4bb68917e15d9
Ivan <ivan@eriad.com>
2015-09-25 18:56:13 +0800

    ok added location

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const fetch       = require('./helper/fetch');
 const token       = require('./key');
 
 
-const bot    = new TelegramBot(token, {polling: true});
-const help   = require('./commands/help')(bot);
-const negeri = require('./commands/negeri')(bot);
-const areas  = require('./commands/areas')(bot);
-const api    = require('./commands/api')(bot);
+const bot      = new TelegramBot(token, {polling: true});
+const help     = require('./commands/help')(bot);
+const negeri   = require('./commands/negeri')(bot);
+const areas    = require('./commands/areas')(bot);
+const api      = require('./commands/api')(bot);
+const location = require('./commands/location')(bot);
 
 
 bot.on('text', function (msg) {
@@ -21,5 +22,17 @@ bot.on('text', function (msg) {
   if (text.indexOf('/negeri') === 0) { return fetch(msg, negeri); }
   if (text.indexOf('/areas')  === 0) { return fetch(msg, areas);  }
   if (text.indexOf('/api')    === 0) { return fetch(msg, api);    }
+
+  if (text.indexOf('/start')  === 0) {
+    bot.sendMessage(msg.chat.id, ' Send us your location or just /help for more commands');
+  }
+
+});
+
+
+bot.on('location', function (msg) {
+
+  console.log(msg);
+  fetch(msg, location);
 
 });
